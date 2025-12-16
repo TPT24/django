db2c46eb14ea4a1a4a586bf3f09ccc86ecf4fe81
TPT24 <tpthinha12k4@gmail.com>
2025-12-16 17:08:23 +0700

    Add Procfile for Railway

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-c8ba!18k9ybgimy)m2z#u*d&u$gs$(k4)d3ge&c*0kr1eoo$v4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", ".railway.app"]
 
 
 # Application definition
