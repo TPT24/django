4ac5a1121bf977a23cf29fc1e1f58672ddbd5dfb
TPT24 <tpthinha12k4@gmail.com>
2025-12-16 17:48:35 +0700

    Initial Django project

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,22 +1,32 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = [
-        "email",
-        "username",
-        "age",
-        "is_staff",
-    ]
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
+
+    list_display = ("email", "username", "age", "is_staff")
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal info", {"fields": ("age",)}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "age", "password1", "password2"),
+        }),
+    )
+
+    search_fields = ("email", "username")
+    ordering = ("email",)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
